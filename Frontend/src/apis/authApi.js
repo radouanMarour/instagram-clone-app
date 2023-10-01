253899e19bae770bb2b4738c25cd37f93b889cb1
radouanMarour <radouanmr999@gmail.com>
2023-10-01 14:41:47 +0100

    update the url endpoint

--- Frontend/src/apis/authApi.js
+++ Frontend/src/apis/authApi.js
@@ -1,9 +1,9 @@
 import api from '../client';
 
 export const register = (body) => {
-    return api.post("http://localhost:5000/api/auth/signup", body);
+    return api.post("/auth/signup", body);
 }
 
 export const login = async (body) => {
-    return api.post("http://localhost:5000/api/auth/login", body);
+    return api.post("/auth/login", body);
 }
