817fd7037618cf3fca5fa59f60428af0a7a57770
radouanMarour <radouanmr999@gmail.com>
2025-02-06 18:11:25 +0100

    add api base url

--- Frontend/src/client.js
+++ Frontend/src/client.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
-// const instance = axios.create({
-//     baseURL: process.env.REACT_APP_API_URL,
-// })
-
 const instance = axios.create({
-    baseURL: 'http://localhost:5000',
+    baseURL: import.meta.env.VITE_API_BASE_URL,
 })
+
+// const instance = axios.create({
+//     baseURL: 'http://localhost:5000',
+// })
 
 export default instance;
